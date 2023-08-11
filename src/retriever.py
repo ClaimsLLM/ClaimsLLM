dd7bbc7143ded8ec5602d3c4cdf161bc809c830a
sancharee-das <sancharee.das@goml.io>
2023-08-11 10:34:32 +0530

    added changes

--- src/retriever.py
+++ src/retriever.py
@@ -47,11 +47,13 @@ def run_db_build():
     vectorstore = FAISS.from_documents(texts, embeddings)
     vectorstore.save_local(cfg.DB_FAISS_PATH)
 
-def run_db_build_claims():
-    loader = DirectoryLoader(cfg.DATA_PATH,
-                             glob='*.pdf',
-                             loader_cls=PyPDFLoader)
+def run_db_build_claims(file):
+    loader=PyPDFLoader(file)
     documents = loader.load()
+    # loader = DirectoryLoader(cfg.DATA_PATH,
+    #                          glob='*.pdf',
+    #                          loader_cls=PyPDFLoader)
+
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=cfg.CHUNK_SIZE,
                                                    chunk_overlap=cfg.CHUNK_OVERLAP)
     texts = text_splitter.split_documents(documents)
